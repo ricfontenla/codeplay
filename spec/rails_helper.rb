40d7360b3b45feb4c6685b59c6aa1a96dd9e6fb1
Ricardo Fontenla <ric_parada@hotmail.com>
2021-05-13 01:24:42 -0300

    Visitante visita a homepage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,7 @@ require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-require 'capybara/spec'
+require 'capybara/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -32,6 +32,10 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  config.before(type: :system) do
+    driven_by :rack_test
+  end
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
