6918743708749d8db7833c90f8acdda76c8088d9
Ricardo Fontenla <ric_parada@hotmail.com>
2021-05-26 00:19:54 -0300

    Confirm to deletes, and alteration in buttons with bootstrap

--- spec/system/visitor_visit_homepage_spec.rb
+++ spec/system/visitor_visit_homepage_spec.rb
@@ -4,7 +4,6 @@ describe 'Visitor visit homepage' do
   it 'successfully' do
     visit root_path
 
-    expect(page).to have_css('h1', text: 'Codeplay')
     expect(page).to have_css('h3', text: 'Boas vindas ao sistema de gestão de '\
                                          'cursos e aulas')
   end
